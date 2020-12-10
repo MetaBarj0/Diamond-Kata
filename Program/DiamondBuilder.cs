999daa980904fb6be52050e04f7016af3fa9e25c
Sébastien Levy <sebastien.levy@cdbdx.biz>
2020-12-10 08:49:44 +0100

    step 006 - refacto done
    - created specific failing method for invalid input

--- Program/DiamondBuilder.cs
+++ Program/DiamondBuilder.cs
@@ -9,14 +9,38 @@ namespace Program
         public static string MakeDiamondWith(string input)
         {
             if (input == null)
-                throw new ForbiddenNullInputException(@"Making a diamond with a null input does not make sense!
+                FailForNullInput();
+
+            FailForEmptyInput();
+
+            throw new NotImplementedException();
+        }
+
+        private static void FailForNullInput()
+        {
+            StringBuilder messageBuilder = new StringBuilder();
+
+            messageBuilder.AppendLine("Making a diamond with a null input does not make sense!");
+            messageBuilder.Append(@"
 Usage: diamond letter
 where letter is a valid uppercase or lowercase letter.");
 
-            throw new ForbiddenEmptyInputException(@"Making a diamond with an empty input does not make sense!
-Usage: diamond letter
-where letter is a valid uppercase or lowercase letter.");
+            throw new ForbiddenNullInputException(messageBuilder.ToString());
         }
+
+        private static void FailForEmptyInput()
+        {
+            StringBuilder messageBuilder = new StringBuilder();
+
+            messageBuilder.AppendLine("Making a diamond with an empty input does not make sense!");
+            messageBuilder.Append(USAGE);
+
+            throw new ForbiddenEmptyInputException(messageBuilder.ToString());
+        }
+
+        private const string USAGE = @"
+Usage: diamond letter
+where letter is a valid uppercase or lowercase letter.";
     }
 
     public class ForbiddenNullInputException : Exception
